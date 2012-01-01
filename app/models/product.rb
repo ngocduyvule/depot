e3c7540b19e09a90f647084bdef6bfed920f0f73
LE Ngoc-Duy-Vu <foenix.skies@gmail.com>
2012-01-02 00:45:53 +0100

    Iteration D2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
 	default_scope :order => 'title'
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true
@@ -10,4 +13,16 @@ class Product < ActiveRecord::Base
 	validates :image_url, :format => {
 		:with		=> %r{\.(gif|jpg|png)$}i,
 		:message	=> "Doit-être un format d'image valide, tel que JPG, GIF, PNG"}
+		
+	private
+		
+		# ensure that there are no line items referencing this product
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
 end
