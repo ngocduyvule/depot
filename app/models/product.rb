f14816b059d9de9c93d17b02735eb2ece1155d4f
LE Ngoc-Duy-Vu <foenix.skies@gmail.com>
2012-01-01 15:39:16 +0100

    Validation with playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
+# encoding: utf-8
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true
+	validates :title, :length => { 
+		:minimum 	=> 10,
+		:message	=> 'Doit comporter au moins 10 caractères'}
 	validates :image_url, :format => {
 		:with		=> %r{\.(gif|jpg|png)$}i,
-		:message	=> 'Doit-etre un format d\'image valide, tel que JPG, GIF, PNG'
-	}
+		:message	=> "Doit-être un format d'image valide, tel que JPG, GIF, PNG"}
 end
